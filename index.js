88e639c1742401c3e1af652e9ac40444ff5b8f5b
Ville Lohkovuori <villelohkovuori1@gmail.com>
2023-09-20 22:46:42 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://en.wikipedia.org/wiki/';
+
+const wikiRequest = async (articleUrl) => {
+  // TODO
+}
+
+const printLinkTree = async (startingArticle, treeDepth, followFirstLinks) => {
+
+};
