46571975abb5219454b7ce4ddd9c566c16932072
Ville Lohkovuori <villelohkovuori1@gmail.com>
2023-09-20 23:19:04 +0300

    add skeletons of helper functions. work on logic.

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const wikiRequest = async (articleUrl) => {
   // TODO
 }
 
-const printLinkTree = async (startingArticle, treeDepth, followFirstLinks) => {
+const getLink = (wikiArticle, ordinal) => {
+  // TODO
+}
+
+const printLink = (link, indentation) => {
+  const spaces = ' '.repeat(indentation)
+  console.log(`${spaces}${link}`)
+}
+
+const printLinkTree = async (startingArticle, linkDepth = 1, followFirstLinks = 1) => {
+  if (!startingArticle || typeof startingArticle !== 'string') {
+    console.log('Error! Call printLinkTree() with the url-name of a Wikipedia article.')
+    return
+  }
+  if (typeof linkDepth !== 'number' || typeof followFirstLinks !== 'number') {
+    console.log('Error! Arguments after the first one must be numbers.')
+    return
+  }
+
+  for (let i = 0; i < linkDepth; i++) {
+
+  }
+
 
 };
