e616b289591c11ee2af9e7d5b51e8e9a3caff322
Ville Lohkovuori <villelohkovuori1@gmail.com>
2023-09-21 18:23:37 +0300

    complete main function logic

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const BASE_URL = 'https://en.wikipedia.org/wiki/';
 
-const wikiRequest = async (articleUrl) => {
+const getLink = async (articleUrl, linkOrdinal) => {
   // TODO
-}
-
-const getLink = (wikiArticle, ordinal) => {
-  // TODO
-}
+};
 
 const printLink = (link, indentation) => {
-  const spaces = ' '.repeat(indentation)
-  console.log(`${spaces}${link}`)
-}
-
-const printLinkTree = async (startingArticle, linkDepth = 1, followFirstLinks = 1) => {
-  if (!startingArticle || typeof startingArticle !== 'string') {
-    console.log('Error! Call printLinkTree() with the url-name of a Wikipedia article.')
-    return
-  }
-  if (typeof linkDepth !== 'number' || typeof followFirstLinks !== 'number') {
-    console.log('Error! Arguments after the first one must be numbers.')
-    return
-  }
-
-  for (let i = 0; i < linkDepth; i++) {
-
-  }
-
-
+  const spaces = '  '.repeat(indentation);
+  console.log(`${spaces}${link}`);
 };
+
+const printLinkTree = async (articleUrl, linkDepth = 1, followLinks = 1, indentation = 0) => {
+  if (!articleUrl || typeof articleUrl !== 'string') {
+    console.log('Error! Call printLinkTree() with the url-name of a Wikipedia article.');
+    return;
+  }
+  if (typeof linkDepth !== 'number' || typeof followLinks !== 'number') {
+    console.log('Error! Arguments after the first one must be numbers.');
+    return;
+  }
+  printLink(articleUrl, indentation);
+  if (linkDepth <= 0) return;
+
+  for (let i = 0; i < followLinks; i++) {
+    const wikiArticle = await getLink(articleUrl, i);
+
+    if (wikiArticle) { 
+      await printLinkTree(wikiArticle, linkDepth - 1, followLinks, indentation + 1);
+     } else { 
+      break; 
+    }
+  }
+};
+
+printLinkTree('Light', 2, 3);
