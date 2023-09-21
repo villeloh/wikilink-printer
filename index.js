26009296b99c5826f656d3a5b750bea388d493cd
Ville Lohkovuori <villelohkovuori1@gmail.com>
2023-09-21 19:12:54 +0300

    add wiki fetch

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-const BASE_URL = 'https://en.wikipedia.org/wiki/';
+import axios from 'axios';
 
-const getLink = async (articleUrl, linkOrdinal) => {
-  // TODO
+const BASE_URL = 'https://en.wikipedia.org/w/api.php?';
+
+// to avoid excessive requests
+const cache = {};
+
+const getLink = async (articleUrl, linkIndex) => {
+  try {
+    let extract;
+
+    // Check if article is already in cache
+    if (cache[articleUrl]) {
+      extract = cache[articleUrl];
+    } else {
+      const params = new URLSearchParams({
+        action: 'query',
+        format: 'json',
+        titles: articleUrl,
+        prop: 'extracts',
+        exintro: 'true'
+      });
+
+      const { data } = await axios.get(`${BASE_URL}${params.toString()}`, {
+        headers: { 'Api-User-Agent': 'Ville_L' }
+      });
+      const pages = data?.query?.pages;
+      if (!pages) return null;
+      const firstPage = Object.values(pages)[0];
+      extract = firstPage?.extract;
+      
+      cache[articleUrl] = extract;
+    }
+
+    const dom = new DOMParser();
+    const document = dom.parseFromString(extract, 'text/html');
+    const links = document.querySelectorAll('a');
+
+    if (links.length === 0 || linkIndex >= links.length) return null;
+    const linkElement = links[linkIndex];
+    
+    return linkElement.getAttribute('href')?.startsWith('/') ?
+      `https://en.wikipedia.org${linkElement.getAttribute('href')}` : 
+      linkElement.getAttribute('href');
+  } catch (error) {
+    console.error(`Failed to fetch article: ${error}`)
+    return null
+  }
 };
 
 const printLink = (link, indentation) => {
